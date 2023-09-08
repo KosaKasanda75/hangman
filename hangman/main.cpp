77ce1498f23f6d53d5df837b3cc61060c0914da8
Kosa <kosa.zk@gmail.com>
2023-09-08 15:45:04 +0100

    Initial Commit

---
+++ hangman/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hangman
+//
+//  Created by Kosa Kasanda on 08.09.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
